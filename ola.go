0c9dd4bdc8759063826fd2b385debf829a1ae3e9
André Leoni <andreluizleoni@gmail.com>
2020-02-13 22:45:00 -0300

    Continue refactoring

--- ola.go
+++ ola.go
@@ -2,16 +2,28 @@ package main
 
 import "fmt"
 
+const frances = "frances"
+const espanhol = "espanhol"
 const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
 
-func Ola(nome string) string {
+func Ola(nome string, idioma string) string {
 	if nome == "" {
 		nome = "mundo"
+	}
+
+	if idioma == espanhol {
+		return prefixoOlaEspanhol + nome
+	}
+
+	if idioma == frances {
+		return prefixoOlaFrances + nome
 	}
 
 	return prefixoOlaPortugues + nome
 }
 
 func main() {
-	fmt.Println(Ola("mundo"))
+	fmt.Println(Ola("mundo", ""))
 }
