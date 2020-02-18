9d32be31d71cb5dac2a4fe8bdfddc71ccf7fbd32
André Leoni <andreluizleoni@gmail.com>
2020-02-18 20:24:02 -0300

    continues

--- dicionario/dicionario.go
+++ dicionario/dicionario.go
@@ -4,12 +4,29 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrNaoEncontrado = errors.New("Não foi possível encontrar a palavra.")
+var ErrPalavraExistente = errors.New("Palavra já existe.")
+
 func (d Dictionary) Busca(palavra string) (string, error) {
 	definicao, existe := d[palavra]
 
 	if !existe {
-		return "", errors.New("Não foi possível encontrar a palavra.")
+		return "", ErrNaoEncontrado
 	}
 
 	return definicao, nil
+}
+
+func (d Dictionary) Add(palavra, definicao string) error {
+	_, err := d.Busca(palavra)
+	switch err {
+	case ErrNaoEncontrado:
+		d[palavra] = definicao
+	case nil:
+		return ErrPalavraExistente
+	default:
+		return err
+	}
+
+	return nil
 }
--- dicionario/dicionario_test.go
+++ dicionario/dicionario_test.go
@@ -13,11 +13,34 @@ func TestBusca(t *testing.T) {
 	})
 
 	t.Run("palavra desconhecida", func(t *testing.T) {
-		_, err := dicionario.Busca("desconhecida")
+		_, resultado := dicionario.Busca("desconhecida")
 
-		if err == nil {
-			t.Fatal("é esperado que um erro seja obtido.")
+		comparaErro(t, resultado, ErrNaoEncontrado)
+	})
+}
+
+func TestAdiciona(t *testing.T) {
+	t.Run("palavra nova", func(t *testing.T) {
+		dicionario := Dictionary{}
+		dicionario.Add("teste", "apenas um teste")
+
+		esperado := "apenas um teste"
+		resultado, err := dicionario.Busca("teste")
+
+		if err != nil {
+			t.Fatal("não foi possivel encontrar a palavra adicionada")
 		}
+
+		comparaStrings(t, esperado, resultado)
+	})
+
+	t.Run("palavra existente", func(t *testing.T) {
+		palavra := "teste"
+		definicao := "isso é apenas um teste"
+		dicionario := Dictionary{palavra: definicao}
+		err := dicionario.Add(palavra, "teste novo")
+
+		comparaErro(t, err, ErrPalavraExistente)
 	})
 }
 
@@ -26,5 +49,13 @@ func comparaStrings(t *testing.T, resultado, esperado string) {
 
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s', dado '%s'", resultado, esperado, "test")
+	}
+}
+
+func comparaErro(t *testing.T, resultado, esperado error) {
+	t.Helper()
+
+	if resultado != esperado {
+		t.Errorf("resultado erro '%s', esperado '%s'", resultado, esperado)
 	}
 }
