6e806f53da1d273a389dad863119e83de98ee3bc
André Leoni <andreluizleoni@gmail.com>
2020-02-18 20:11:16 -0300

    continuous dictionary

--- dicionario/dicionario.go
+++ dicionario/dicionario.go
@@ -1,7 +1,15 @@
 package dicionario
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Busca(palavra string) string {
-	return d[palavra]
+func (d Dictionary) Busca(palavra string) (string, error) {
+	definicao, existe := d[palavra]
+
+	if !existe {
+		return "", errors.New("Não foi possível encontrar a palavra.")
+	}
+
+	return definicao, nil
 }
--- dicionario/dicionario_test.go
+++ dicionario/dicionario_test.go
@@ -5,10 +5,20 @@ import "testing"
 func TestBusca(t *testing.T) {
 	dicionario := Dictionary{"test": "isso é apenas um teste"}
 
-	resultado := dicionario.Busca("test")
-	esperado := "isso é apenas um teste"
+	t.Run("palavra conhecida", func(t *testing.T) {
+		resultado, _ := dicionario.Busca("test")
+		esperado := "isso é apenas um teste"
 
-	comparaStrings(t, resultado, esperado)
+		comparaStrings(t, resultado, esperado)
+	})
+
+	t.Run("palavra desconhecida", func(t *testing.T) {
+		_, err := dicionario.Busca("desconhecida")
+
+		if err == nil {
+			t.Fatal("é esperado que um erro seja obtido.")
+		}
+	})
 }
 
 func comparaStrings(t *testing.T, resultado, esperado string) {
