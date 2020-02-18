cbf80771a0d38d6b25afbf3e58ea91c4822ca240
André Leoni <andreluizleoni@gmail.com>
2020-02-18 20:05:15 -0300

    improe dicionario_test

--- dicionario/dicionario_test.go
+++ dicionario/dicionario_test.go
@@ -8,6 +8,12 @@ func TestBusca(t *testing.T) {
 	resultado := Busca(dicionario, "test")
 	esperado := "isso é apenas um teste"
 
+	comparaStrings(t, resultado, esperado)
+}
+
+func comparaStrings(t *testing.T, resultado, esperado string) {
+	t.Helper()
+
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s', dado '%s'", resultado, esperado, "test")
 	}
